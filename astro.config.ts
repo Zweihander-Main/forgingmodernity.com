db41816668f8c766e1430a616d578d43fd89416b
Zweihänder <zweidev@zweihander.me>
2023-03-11 22:32:18 -0500

    build(package): install svelte

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
+import svelte from '@astrojs/svelte';
 
 export default defineConfig({
 	integrations: [
 		image({
 			serviceEntryPoint: '@astrojs/image/sharp',
 		}),
+		svelte(),
 	],
 	vite: {
 		css: {
