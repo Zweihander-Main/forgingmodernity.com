075e29b08622e9a093a31537e53661986ccdbadb
Zweihänder <zweidev@zweihander.me>
2023-03-28 22:46:44 -0400

    build(patch): specify image element ability

--- .typings/leaflet.d.ts
+++
@@ -1,6 +0,0 @@
-import {LatLngBoundsExpression, ImageOverlay, ImageOverlayOptions} from 'leaflet';
-
-declare module 'leaflet' {
-	export function imageOverlay(imageUrl: string | HTMLImageElement, bounds: LatLngBoundsExpression, options?: ImageOverlayOptions): ImageOverlay;
-}
-
