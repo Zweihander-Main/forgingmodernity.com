3f0678894f0544424a152bc6142de4d953744075
Zweihänder <zweidev@zweihander.me>
2023-04-03 22:37:20 -0400

    fix(clouds): abs vs rel path

--- src/util/vars.ts
+++ src/util/vars.ts
@@ -4,13 +4,13 @@ export const MAP_V_GUTTER = 586;
 export const MAP_H_GUTTER = 340;
 export const TOTAL_CLOUDS = 25;
 export const CLOUD_IMAGES: Array<string> = [
-	'img/clouds/cloud1',
-	'img/clouds/cloud2',
-	'img/clouds/cloud3',
-	'img/clouds/cloud4',
-	'img/clouds/cloud5',
-	'img/clouds/cloud6',
-	'img/clouds/cloud7',
+	'/img/clouds/cloud1',
+	'/img/clouds/cloud2',
+	'/img/clouds/cloud3',
+	'/img/clouds/cloud4',
+	'/img/clouds/cloud5',
+	'/img/clouds/cloud6',
+	'/img/clouds/cloud7',
 ];
 export const AVIF_BASE64 =
 	'AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUIAAADybWV0YQAAAAAAAAAoaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGxpYmF2aWYAAAAADnBpdG0AAAAAAAEAAAAeaWxvYwAAAABEAAABAAEAAAABAAABGgAAAB0AAAAoaWluZgAAAAAAAQAAABppbmZlAgAAAAABAABhdjAxQ29sb3IAAAAAamlwcnAAAABLaXBjbwAAABRpc3BlAAAAAAAAAAIAAAACAAAAEHBpeGkAAAAAAwgICAAAAAxhdjFDgQ0MAAAAABNjb2xybmNseAACAAIAAYAAAAAXaXBtYQAAAAAAAAABAAEEAQKDBAAAACVtZGF0EgAKCBgANogQEAwgMg8f8D///8WfhwB8+ErK42A=';
