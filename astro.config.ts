3499ac4bb053a222905f296c4649e26ec649eea0
Zweihänder <zweidev@zweihander.me>
2023-04-12 16:51:24 -0400

    build(postcss): add in nano/purge

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 import svelte from '@astrojs/svelte';
+import purgecss from 'astro-purgecss';
 
+// https://astro.build/config
 export default defineConfig({
 	integrations: [
 		image({
 			serviceEntryPoint: '@astrojs/image/sharp',
 		}),
 		svelte(),
+		purgecss({
+			fontFace: false,
+			variables: true,
+			safelist: [/leaflet-pane/, /leaflet-map/],
+		}),
 	],
 	vite: {
 		css: {
